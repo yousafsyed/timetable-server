9843d5293c968179c2e37469774d13bf26944751
yousafsyed-bg <yousaf@brokergenius.com>
2022-03-21 09:27:47 +0100

    Adding file endings

--- src/Controllers/Schedule/CreateScheduleHandler.ts
+++ src/Controllers/Schedule/CreateScheduleHandler.ts
@@ -2,7 +2,7 @@ import { Controller, UseFilters, Body, Post } from '@nestjs/common';
 import { CreateScheduleUseCase } from 'src/Application/CreateScheduleUseCase';
 import { CreateScheduleRequest } from 'src/Application/CreateScheduleRequest';
 import { Schedule } from 'src/Domain/schedule';
-import { MongoErrorFilters } from './MongoErrorFilters';
+import { MongoErrorFilters } from '../../Infrastructure/ExceptionFilters/MongoErrorFilters';
 
 @Controller()
 export class CreateScheduleHandler {
